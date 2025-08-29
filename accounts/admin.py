a5d82a608d806570f7c4bb26b23e405e41d0e0ea
rarciniegas <rrarciniegas@hotmail.com>
2025-08-28 22:41:51 -0400

    custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "phone_number",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("phone_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("phone_number",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
